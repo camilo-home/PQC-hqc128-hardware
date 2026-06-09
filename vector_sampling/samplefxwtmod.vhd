3620e99bb0ec6bd8ed660754f8ddea9f78898c4e
camilo-home <camilo.home@correounivalle.edu.co>
2026-06-09 07:21:10 -0500

    Update samplefxwtmod.vhd

--- vector_sampling/samplefxwtmod.vhd
+++ vector_sampling/samplefxwtmod.vhd
@@ -8,12 +8,18 @@ entity samplefxwtmod is
     rst           : in  std_logic;
 
     start         : in  std_logic;
-    mode          : in  std_logic; -- 0: wt=66, 1: wt=75
 
     shake_din     : in  std_logic_vector(31 downto 0);
     shake_valid   : in  std_logic;
 
-    sample_done          : out std_logic
+    get_bytes     : out std_logic;  -- solicitar 32 bytes de shake256
+
+    -- Interfaz con la pos_mem
+    wr_en : out std_logic;
+    wr_addr : out unsigned(6 downto 0);
+    wr_data : out unsigned(14 downto 0);
+
+    sample_done   : out std_logic
   );
 end entity;
 
@@ -25,9 +31,12 @@ architecture rtl of samplefxwtmod is
   constant RAM_BYTES : integer := 2209;
 
   -- =========================
-  -- BRAM (bitmap)
-  type ram_type is array (0 to RAM_BYTES-1) of std_logic_vector(7 downto 0);
-  signal ram : ram_type := (others => (others => '0'));
+  -- BRAM 
+type ram_type is array (0 to RAM_BYTES-1) of std_logic_vector(7 downto 0);
+signal ram : ram_type := (others => (others => '0'));
+
+attribute ramstyle : string;
+attribute ramstyle of ram : signal is "M20K";
 
   -- =========================
   -- FSM
@@ -40,7 +49,7 @@ architecture rtl of samplefxwtmod is
     COMP_3,
     READ_BRAM,
     WRITE_BRAM,
-    NEXT_I,
+    COUNT_POS,
     DONE
   );
 
@@ -48,8 +57,9 @@ architecture rtl of samplefxwtmod is
 
   -- =========================
   -- DATAPATH
-  signal wt        : integer range 0 to 75 := 0;
-  signal i         : integer range 0 to 75 := 0;
+  signal WT       : integer := 75;
+  signal i         : integer range 0 to WT := 0;
+  signal dup       : integer := 0;
 
   signal u32       : unsigned(31 downto 0);
   signal m         : unsigned(14 downto 0);
@@ -60,7 +70,10 @@ architecture rtl of samplefxwtmod is
   signal byte_addr : integer range 0 to RAM_BYTES-1;
   signal bit_addr  : integer range 0 to 7;
 
-  signal byte_reg  : std_logic_vector(7 downto 0);
+  signal we        : std_logic := '0';
+  signal re        : std_logic := '0';
+  signal din_ram   : std_logic_vector(7 downto 0);
+  signal dout_ram  : std_logic_vector(7 downto 0);
 
 begin
 
@@ -71,9 +84,8 @@ begin
 
       if rst = '1' then
         fsm   <= IDLE;
-        i     <= 0;
-        sample_done  <= '0';
-        ram   <= (others => (others => '0'));
+	i <= 0;
+	sample_done <= '0';
 
       else
         case fsm is
@@ -84,22 +96,18 @@ begin
 
             if start = '1' then
               i <= 0;
-
-              if mode = '0' then
-                wt <= 66;
-              else
-                wt <= 75;
-              end if;
-
-              ram <= (others => (others => '0'));
               fsm <= LOAD_U32;
             end if;
 
           -- =========================
           when LOAD_U32 =>
             if shake_valid = '1' then
+	      get_bytes <= '0';
               u32 <= unsigned(shake_din);
               fsm <= COMP_1;
+	      
+	    else
+	      get_bytes <= '1';
             end if;
 
           -- =========================
@@ -120,37 +128,35 @@ begin
 	      fsm <= COMP_3;
 	    -- escalado (shift) + posicion
 	    when COMP_3 =>
+	      re <= '1';
 	      posn    <= mult(46 downto 32) + to_unsigned(i, 15);
 	      fsm <= READ_BRAM;
 
           -- =========================
           when READ_BRAM =>
-            byte_addr <= to_integer(posn(14 downto 3));
-            bit_addr  <= to_integer(posn(2 downto 0));
-
-            byte_reg <= ram(to_integer(posn(14 downto 3)));
-
+	    re <= '0';
             fsm <= WRITE_BRAM;
 
           -- =========================
           when WRITE_BRAM =>
 
-            if byte_reg(bit_addr) = '0' then
+            if dout_ram(bit_addr) = '0' then
               -- escribir bit
-              byte_reg(bit_addr) <= '1';
-              ram(byte_addr) <= byte_reg;
-
-              fsm <= NEXT_I;
+	      din_ram <= dout_ram;
+              din_ram(bit_addr) <= '1';
+	      we <= '1';
+	      fsm <= COUNT_POS;
             else
               -- duplicado ? ignorar y pedir nuevo u32
+	      dup <= dup + 1;
               fsm <= LOAD_U32;
             end if;
 
           -- =========================
-          when NEXT_I =>
+          when COUNT_POS =>
+	    we <= '0';
             i <= i + 1;
-
-            if i + 1 >= wt then
+            if i + 1 >= WT then
               fsm <= DONE;
             else
               fsm <= LOAD_U32;
@@ -158,6 +164,7 @@ begin
 
           -- =========================
           when DONE =>
+	    dup <= 0;
             sample_done <= '1';
             fsm <= IDLE;
 
@@ -165,5 +172,23 @@ begin
       end if;
     end if;
   end process;
+
+process(clk)
+begin
+  if rising_edge(clk) then
+    if we = '1' then
+      ram(byte_addr) <= din_ram; -- esritura
+    elsif re = '1' then
+      dout_ram <= ram(byte_addr);  -- lectura
+    end if;
+  end if;
+end process;
+
+byte_addr <= to_integer(posn(14 downto 3));
+bit_addr <= to_integer(posn(2 downto 0));
+
+wr_addr <= to_unsigned(i, 7);
+wr_data <= posn(14 downto 0);
+wr_en <= we;
 
 end rtl;
